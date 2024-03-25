c29f41fbb00014d8863018435d9a2ed4d89ec5f2
migayalach <ayalachavezmiguel@gmail.com>
2024-03-25 19:03:47 -0400

    mantenimiento

---
+++ client/src/app/maintenance/page.jsx
@@ -0,0 +1,23 @@
+// COMPONET'S
+
+// HOOK'S
+import React from "react";
+
+// LIBRARY
+
+//REDUX
+
+// JAVASCRIP
+
+// STYLESHEET'S
+
+import React from "react";
+
+function Maintenance() {
+  return (
+    <div>
+      Maintenance
+    </div>
+  );
+}
+export default Maintenance;
