0135bbf2d20d5348cd197ffb6120b90044484789
migayalach <ayalachavezmiguel@gmail.com>
2024-03-25 15:21:28 -0400

    student

---
+++ client/src/app/student/page.jsx
@@ -0,0 +1,20 @@
+// COMPONET'S
+import NavBar from "@/components/navBar/NavBar";
+
+// HOOK'S
+import React from "react";
+
+// LIBRARY
+
+//REDUX
+
+// JAVASCRIP
+
+// STYLESHEET'
+function Student() {
+  return (
+    <div>Student</div>
+  )
+}
+
+export default Student
