1967f57a0ec83ac75c893e7a06e0677c249a4f7a
migayalach <ayalachavezmiguel@gmail.com>
2024-03-25 15:21:01 -0400

    staff

---
+++ client/src/app/staff/page.jsx
@@ -0,0 +1,21 @@
+// COMPONET'S
+import NavBar from "@/components/navBar/NavBar";
+
+// HOOK'S
+import React from "react";
+
+// LIBRARY
+
+//REDUX
+
+// JAVASCRIP
+
+// STYLESHEET'
+
+function Staff() {
+  return (
+    <div>staff</div>
+  )
+}
+
+export default Staff
