e05ec58d5b39bf78ef62dc33e0380c8e5024bbb2
migayalach <ayalachavezmiguel@gmail.com>
2024-03-25 15:20:47 -0400

    payments

---
+++ client/src/app/payments/page.jsx
@@ -0,0 +1,21 @@
+// COMPONET'S
+import NavBar from "@/components/navBar/NavBar";
+
+// HOOK'S
+import React from "react";
+
+// LIBRARY
+
+//REDUX
+
+// JAVASCRIP
+
+// STYLESHEET'
+
+function Payments() {
+  return (
+    <div>Payments</div>
+  )
+}
+
+export default Payments
