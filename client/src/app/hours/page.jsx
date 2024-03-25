28fbd7f1c4369f25151e9618d4bfc4213964c527
migayalach <ayalachavezmiguel@gmail.com>
2024-03-25 15:20:20 -0400

    hours

---
+++ client/src/app/hours/page.jsx
@@ -0,0 +1,21 @@
+// COMPONET'S
+import NavBar from "@/components/navBar/NavBar";
+
+// HOOK'S
+import React from "react";
+
+// LIBRARY
+
+//REDUX
+
+// JAVASCRIP
+
+// STYLESHEET'
+
+function Hours() {
+  return (
+    <div>Hours</div>
+  )
+}
+
+export default Hours
