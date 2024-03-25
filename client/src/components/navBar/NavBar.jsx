33b15647aabe6014b5daa5405852c2fab4d53054
migayalach <ayalachavezmiguel@gmail.com>
2024-03-25 14:18:48 -0400

    componentes

---
+++ client/src/components/navBar/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Personal</li>
+        <li>Estudiantes</li>
+        <li>Mensualidades</li>
+        <li>Horarios</li>
+        <li>Clases</li>
+        <li>Danzas</li>
+        <li>USUARIO</li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
