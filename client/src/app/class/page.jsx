f58798ddc9407cddbdc4853b957072d144d7373c
migayalach <ayalachavezmiguel@gmail.com>
2024-03-25 15:19:34 -0400

    class

---
+++ client/src/app/class/page.jsx
@@ -0,0 +1,26 @@
+// COMPONET'S
+import NavBar from "@/components/navBar/NavBar";
+
+// HOOK'S
+import React from "react";
+
+// LIBRARY
+
+//REDUX
+
+// JAVASCRIP
+
+// STYLESHEET'
+
+function Class() {
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <h1>Class</h1>
+    </div>
+  );
+}
+
+export default Class;
