3b2bb559e36128f29401faf50114047aed1888da
migayalach <ayalachavezmiguel@gmail.com>
2024-03-25 15:20:02 -0400

    home

---
+++ client/src/app/home/page.jsx
@@ -0,0 +1,26 @@
+// COMPONET'S
+import NavBar from "@/components/navBar/NavBar";
+
+// HOOK'S
+import React from "react";
+
+// LIBRARY
+
+//REDUX
+
+// JAVASCRIP
+
+// STYLESHEET'
+
+function Home() {
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <h1>SOY HOME</h1>
+    </div>
+  );
+}
+
+export default Home;
