d7551b10ee878b68201d4b750fdb9ca9e0ad0ff7
migayalach <ayalachavezmiguel@gmail.com>
2024-03-25 19:04:16 -0400

    login

---
+++ client/src/app/login/page.jsx
@@ -0,0 +1,21 @@
+// COMPONET'S
+import NavBar from "@/components/navBar/NavBar";
+
+// HOOK'S
+import React from "react";
+
+// LIBRARY
+
+//REDUX
+
+// JAVASCRIP
+
+// STYLESHEET'
+
+function Login() {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
