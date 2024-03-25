f12aa90b7324510d743929aa90f2245dc7125e5d
migayalach <ayalachavezmiguel@gmail.com>
2024-03-25 15:19:08 -0400

    menu creado con exito

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,18 +1,54 @@
+// COMPONET'S
+
+// HOOK'S
 import React from "react";
+
+// LIBRARY
+
+//REDUX
+
+// JAVASCRIP
+
+// STYLESHEET'
+import "./nav-bar.css"
 
 function NavBar() {
   return (
-    <div>
-      <ul>
-        <li>Home</li>
-        <li>Personal</li>
-        <li>Estudiantes</li>
-        <li>Mensualidades</li>
-        <li>Horarios</li>
-        <li>Clases</li>
-        <li>Danzas</li>
-        <li>USUARIO</li>
-      </ul>
+    <div className="conteiner-navbar">
+      <div>
+        <h3>LOGO</h3>
+      </div>
+      <div className="conteiner-menu">
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/staff">Personal</a>
+          </li>
+          <li>
+            <a href="/student">Estudiantes</a>
+          </li>
+          <li>
+            <a href="/payments">Mensualidades</a>
+          </li>
+          <li>
+            <a href="/hours">Horarios</a>
+          </li>
+          <li>
+            <a href="/class">Clases</a>
+          </li>
+          <li>
+            <a href="/dances">Danzas</a>
+          </li>
+          <li>
+            <a href="/">Salir</a>
+          </li>
+          <li>
+            <a href="">USUARIO</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
