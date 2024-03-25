80d4be39de3cc597e10ba04cfab7fcbbe800f8fe
migayalach <ayalachavezmiguel@gmail.com>
2024-03-25 15:19:49 -0400

    dances

---
+++ client/src/app/dances/page.jsx
@@ -0,0 +1,21 @@
+// COMPONET'S
+import NavBar from "@/components/navBar/NavBar";
+
+// HOOK'S
+import React from "react";
+
+// LIBRARY
+
+//REDUX
+
+// JAVASCRIP
+
+// STYLESHEET'
+
+function Dance() {
+  return (
+    <div>Dance</div>
+  )
+}
+
+export default Dance
