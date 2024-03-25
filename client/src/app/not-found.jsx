7aeafe6d2c6463558fd96df7b79ab98e38755d06
migayalach <ayalachavezmiguel@gmail.com>
2024-03-25 19:02:48 -0400

    pagina para error 404

---
+++ client/src/app/not-found.jsx
@@ -0,0 +1,25 @@
+// COMPONET'S
+
+// HOOK'S
+import React from "react";
+import Link from "next/link";
+
+// LIBRARY
+
+//REDUX
+
+// JAVASCRIP
+
+// STYLESHEET'S
+
+export default function NotFuntion() {
+  return (
+    <div>
+      <div>
+        <div>404</div>
+        <h3>Pagina no encontrada</h3>
+      </div>
+      <Link href="/home">Volver</Link>
+    </div>
+  );
+}
