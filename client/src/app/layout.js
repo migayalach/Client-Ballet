aee6b9a3530522b61681913f8a168c2b5d6358c0
migayalach <ayalachavezmiguel@gmail.com>
2024-03-26 21:18:58 -0400

    exportacion de la tienda a la raiz del proyecto

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "../stylesheet/globals.css";
-import store from "../redux/store";
+import store from "@/redux/store";
 import { Provider } from "react-redux";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider store={store}>{children}</Provider>
+      </body>
     </html>
   );
 }
