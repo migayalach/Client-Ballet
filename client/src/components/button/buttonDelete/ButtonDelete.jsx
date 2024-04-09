c6f44c3c815a876510d823b937745ad0fbf61a97
migayalach <ayalachavezmiguel@gmail.com>
2024-04-08 23:54:40 -0400

    button

---
+++ client/src/components/button/buttonDelete/ButtonDelete.jsx
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from "react";
+import { Button, Flex } from "antd";
+import Text from "../../text/Text";
+import DeleteModal from "@/components/modal/deleteModal/DeleteModal";
+
+function ButtonDelete({ idData, text }) {
+  const [flag, setFlag] = useState(false);
+  
+  const handleChange = () => {
+    setFlag(!flag);
+  };
+
+  return (
+    <Flex wrap="wrap" gap="small">
+      <Button type="primary" onClick={handleChange} danger>
+        <Text text={text} />
+        {
+          flag && <DeleteModal idData={idData}/>
+        }
+      </Button>
+    </Flex>
+  );
+}
+
+export default ButtonDelete;
