63240b8d31ff84e4dbcb8d852ba6f4650b7e5624
migayalach <ayalachavezmiguel@gmail.com>
2024-04-08 23:52:27 -0400

    redux

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,1 +1,65 @@
+import axios from "axios";
+import {
+  postHours,
+  getAllHours,
+  getIdHours,
+  errorResponse,
+  deleteIdHours,
+} from "./slice";
+const URL = "http://localhost:3001/academy";
+
 // ACCIONES A EJECUTARSE
+export const createHours = (infoData) => {
+  return async function (dispatch) {
+    try {
+      const data = (await axios.post(`${URL}/hours`, infoData)).data;
+      return dispatch(postHours(data));
+    } catch (error) {
+      return dispatch(errorResponse(error.message));
+    }
+  };
+};
+
+export const getHoursAll = () => {
+  return async function (dispatch) {
+    try {
+      const data = (await axios.get(`${URL}/hours`)).data;
+      return dispatch(getAllHours(data));
+    } catch (error) {
+      return dispatch(errorResponse(error.message));
+    }
+  };
+};
+
+export const getPageHours = (page) => {
+  return async function (dispatch) {
+    try {
+      const data = (await axios.get(`${URL}/hours?page=${page}`)).data;
+      return dispatch(getAllHours(data));
+    } catch (error) {
+      return dispatch(errorResponse(error.message));
+    }
+  };
+};
+
+export const getByIdHours = (idHours) => {
+  return async function (dispatch) {
+    try {
+      const data = (await axios.get(`${URL}/hours/${idHours}`)).data;
+      return dispatch(getIdHours(data));
+    } catch (error) {
+      return dispatch(errorResponse(error.message));
+    }
+  };
+};
+
+export const removeIdHours = (idHours) => {
+  return async function (dispatch) {
+    try {
+      const data = (await axios.delete(`${URL}/hours/${idHours}`)).data;
+      return dispatch(deleteIdHours(data));
+    } catch (error) {
+      return dispatch(errorResponse(error.message));
+    }
+  };
+};
