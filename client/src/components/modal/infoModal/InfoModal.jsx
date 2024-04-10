12b948063520d7a1e48f012f7dd5d44fa27c6de6
migayalach <ayalachavezmiguel@gmail.com>
2024-04-09 20:39:51 -0400

    info modal

--- client/src/components/modal/infoModal/InfoModal.jsx
+++ client/src/components/modal/infoModal/InfoModal.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Button, Modal } from "antd";
 
-function InfoModal() {
+function InfoModal({ flag, handleInfo }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+    handleInfo(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+    handleInfo(false);
+  };
+  
+  useEffect(() => {
+    setIsModalOpen(true);
+    handleInfo;
+  }, [flag]);
+
   return (
-    <div>InfoModal</div>
-  )
+    <>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </>
+  );
 }
 
-export default InfoModal
+export default InfoModal;
