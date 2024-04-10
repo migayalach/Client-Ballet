a59045862d67698f44268da66358e011ec228003
migayalach <ayalachavezmiguel@gmail.com>
2024-04-10 19:38:14 -0400

    edit modal

--- client/src/components/modal/editModal/EditModal.jsx
+++ client/src/components/modal/editModal/EditModal.jsx
@@ -1,9 +1,10 @@
 // COMPONET'S
 import FormComponent from "@/components/form/FormComponent";
+import Text from "@/components/text/Text";
 
 // HOOK'S
 import React, { useState, useEffect } from "react";
-import { Modal } from "antd";
+import { Modal, Button } from "antd";
 
 // LIBRARY
 
@@ -13,8 +14,12 @@ import { Modal } from "antd";
 
 // STYLESHEET'
 
-function EditModal({ idData }) {
+function EditModal({ idData, text }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
 
   const handleOk = () => {
     setIsModalOpen(false);
@@ -23,13 +28,13 @@ function EditModal({ idData }) {
   const handleCancel = () => {
     setIsModalOpen(false);
   };
-
-  useEffect(() => {
-    setIsModalOpen(true);
-  }, [idData]);
-
+  
   return (
     <>
+      <Button type="primary" onClick={showModal}>
+        <Text text={text} />
+      </Button>
+
       <Modal
         title="Editar informacion"
         open={isModalOpen}
