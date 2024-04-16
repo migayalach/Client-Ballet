90d38658993b1e43a8a73631fdb1d42a7c07c194
migayalach <ayalachavezmiguel@gmail.com>
2024-04-16 12:05:34 -0400

    formulario para staff, solo falta la foto para lo cual se usara cloudinary

--- client/src/components/form/formStaff/FormComponet.jsx
+++ client/src/components/form/formStaff/FormComponet.jsx
@@ -1,17 +1,32 @@
+// COMPONET'S
+import SelectComponet from "@/components/select/SelectComponet";
+import Text from "@/components/text/Text";
+
+// HOOK'S
 import React, { useState, useEffect } from "react";
+
+// LIBRARY
+import customParseFormat from "dayjs/plugin/customParseFormat";
 import { PlusOutlined } from "@ant-design/icons";
-import { Button, DatePicker, Form, Input, Select, Switch, Upload } from "antd";
-import { getExtensionAll, getLevelAll } from "@/redux/actions";
+import { Button, DatePicker, Form, Input, Switch, Upload } from "antd";
+import dayjs from "dayjs";
+
+//REDUX
+import { getExtensionAll, getLevelAll, createStaff } from "@/redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 
-const { TextArea } = Input;
+// STYLESHEET'
 
+// JAVASCRIP
+const { TextArea } = Input;
 const normFile = (event) => {
   if (Array.isArray(event)) {
     return event;
   }
   return event?.fileList;
 };
+
+dayjs.extend(customParseFormat);
 
 function FormComponet() {
   const dispatch = useDispatch();
@@ -28,20 +43,35 @@ function FormComponet() {
     dateBirthStaff: "",
     carnetStaff: 0,
     photoStaff: "",
-    stateStaff: false,
+    stateStaff: true,
   });
 
   const handleChange = (event) => {
+    const key = !event.key ? [event.target.name] : event.title;
     setData({
       ...data,
-      [event.target.name]: event.target.value,
+      [key]: !event.key ? event.target.value : event.key,
     });
+  };
+
+  const onChangeDate = (date, dateString) => {
+    setData({
+      ...data,
+      dateBirthStaff: dateString,
+    });
+  };
+
+  const onFinish = () => {
+    dispatch(createStaff(data));
   };
 
   useEffect(() => {
     dispatch(getExtensionAll());
     dispatch(getLevelAll());
   }, []);
+
+  // useEffect(() => {
+  // }, [onFinish]);
 
   return (
     <>
@@ -56,36 +86,71 @@ function FormComponet() {
         style={{
           maxWidth: 600,
         }}
+        onFinish={onFinish}
       >
         <Form.Item label="Nombres">
-          <Input />
+          <Input
+            onChange={handleChange}
+            name="nameStaff"
+            placeholder="Alverto Reinaldo"
+          />
         </Form.Item>
+
         <Form.Item label="Apellidos">
-          <Input />
+          <Input
+            onChange={handleChange}
+            name="lastNameStaff"
+            placeholder="Del Rio"
+          />
         </Form.Item>
+
         <Form.Item label="Email">
-          <Input />
+          <Input
+            onChange={handleChange}
+            name="emailStaff"
+            placeholder="albert@gmail.com"
+          />
         </Form.Item>
+
         <Form.Item label="Carnet">
-          <Input />
+          <Input
+            onChange={handleChange}
+            name="carnetStaff"
+            placeholder="8569134"
+          />
         </Form.Item>
 
         <Form.Item label="Extension">
-          <Select>
-            <Select.Option value="demo">Demo</Select.Option>
-          </Select>
+          <SelectComponet
+            list={selectExtension}
+            handleChange={handleChange}
+            flag="Extension"
+          />
         </Form.Item>
+
         <Form.Item label="Nivel">
-          <Select>
-            <Select.Option value="demo">Demo</Select.Option>
-          </Select>
+          <SelectComponet
+            list={selectLevel}
+            handleChange={handleChange}
+            flag="Level"
+          />
         </Form.Item>
 
         <Form.Item label="Fecha de nacimiento">
-          <DatePicker />
+          <DatePicker
+            placeholder={"2019-09-03"}
+            name="dateBirthStaff"
+            onChange={onChangeDate}
+          />
         </Form.Item>
+
         <Form.Item label="Dirección">
-          <TextArea rows={4} />
+          <TextArea
+            rows={4}
+            onChange={handleChange}
+            name="addressStaff"
+            placeholder="Calle Siempre Viva N°666"
+          />
         </Form.Item>
 
         <Form.Item label="Estado" valuePropName="checked">
@@ -114,6 +179,10 @@ function FormComponet() {
             </button>
           </Upload>
         </Form.Item>
+
+        <Button type="primary" htmlType="submit">
+          <Text text="Crear" />
+        </Button>
       </Form>
     </>
   );
