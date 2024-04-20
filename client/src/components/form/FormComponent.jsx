9c35e5b371c3c3375a9c3bb20e674ea7512ca1c2
migayalach <ayalachavezmiguel@gmail.com>
2024-04-19 23:53:11 -0400

    formularios

---
+++ client/src/components/form/FormComponent.jsx
@@ -0,0 +1,153 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import State from "../state/State";
+import { createHours, editIdHours } from "@/redux/actions";
+import { Button, Form } from "antd";
+import { timeDifference } from "@/utils/calHours";
+import TotalHours from "../totalHours/TotalHours";
+import { getByIdHours } from "@/redux/actions";
+
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+
+const config = {
+  // rules: [
+  //   {
+  //     type: "object",
+  //     required: true,
+  //     message: "Por favor seleccione una hora!",
+  //   },
+  // ],
+};
+
+const FormComponent = ({ idData, option, handleState }) => {
+  const dispatch = useDispatch();
+  const dataHours = useSelector((state) => state.root);
+
+  const [data, setData] = useState({
+    startTime: "00:00:00",
+    endTime: "00:00:00",
+    totalTime: "00:00:00",
+    stateHours: false,
+  });
+
+  const handleChange = (name, time, timeString, value) => {
+    setData({
+      ...data,
+      [name]: name !== "stateHours" ? timeString : value,
+    });
+  };
+
+  const onFinish = () => {
+    option === "edit"
+      ? dispatch(editIdHours({ ...data, idHours: idData }))
+      : dispatch(createHours(data)) && handleState();
+  };
+
+  useEffect(() => {
+    if (idData) {
+      dispatch(getByIdHours(idData));
+    }
+  }, [idData]);
+
+  useEffect(() => {
+    if (option === "edit" && dataHours.data) {
+      setData({
+        startTime: dataHours.data.startTime,
+        endTime: dataHours.data.endTime,
+        totalTime: dataHours.data.totalTime,
+        stateHours: dataHours.data.stateHours,
+      });
+    }
+  }, [dataHours, option]);
+
+  useEffect(() => {
+    if (data.startTime !== "00:00:00" && data.endTime !== "00:00:00") {
+      handleChange(
+        "totalTime",
+        "",
+        timeDifference(data.startTime, data.endTime)
+      );
+    }
+  }, [data.endTime, data.startTime]);
+
+  return (
+    <Form
+      name=""
+      {...formItemLayout}
+      onFinish={onFinish}
+      style={{
+        maxWidth: 650,
+      }}
+    >
+      <Form.Item name="time-start" label="Inicio de clase" {...config}>
+        <TotalHours
+          name="startTime"
+          hours={data.startTime}
+          handleChange={handleChange}
+        />
+      </Form.Item>
+
+      <Form.Item name="time-end" label="Finalización de clase" {...config}>
+        <TotalHours
+          name="endTime"
+          hours={data.endTime}
+          handleChange={handleChange}
+        />
+      </Form.Item>
+
+      <Form.Item name="duration" label="Duración de clase">
+        <TotalHours name="total" hours={data.totalTime} />
+      </Form.Item>
+
+      {option === "edit" && (
+        <Form.Item
+          name="switch"
+          label="Estado de clase"
+          valuePropName="checked"
+        >
+          <State
+            name="stateHours"
+            stateHours={data.stateHours}
+            handleChange={handleChange}
+          />
+        </Form.Item>
+      )}
+
+      <Form.Item
+        wrapperCol={{
+          xs: {
+            span: 24,
+            offset: 0,
+          },
+          sm: {
+            span: 16,
+            offset: 8,
+          },
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Aceptar
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default FormComponent;
---
+++ client/src/components/form/formStaff/FormComponet.jsx
@@ -0,0 +1,261 @@
+// COMPONET'S
+import SelectComponet from "@/components/select/SelectComponet";
+import Text from "@/components/text/Text";
+
+// HOOK'S
+import React, { useState, useEffect } from "react";
+
+// LIBRARY
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import { PlusOutlined } from "@ant-design/icons";
+import { Button, DatePicker, Form, Input, Switch, Upload } from "antd";
+import dayjs from "dayjs";
+
+//REDUX
+import {
+  getExtensionAll,
+  getLevelAll,
+  createStaff,
+  createHours,
+  getByIdStaff,
+  editStaff,
+} from "@/redux/actions";
+import { useDispatch, useSelector } from "react-redux";
+import PhotoLoading from "@/components/photoLoading/PhotoLoading";
+
+// STYLESHEET'
+
+// JAVASCRIP
+const { TextArea } = Input;
+const normFile = (event) => {
+  if (Array.isArray(event)) {
+    return event;
+  }
+  return event?.fileList;
+};
+
+dayjs.extend(customParseFormat);
+
+function FormComponet({ idData, option, handleState }) {
+  const dispatch = useDispatch();
+  const selectLevel = useSelector(({ root }) => root?.level);
+  const selectExtension = useSelector(({ root }) => root?.extension);
+  const selectIdStaff = useSelector(({ root }) => root?.data);
+  const [data, setData] = useState({
+    idLevel: 0,
+    idExtension: 0,
+    nameStaff: "",
+    lastNameStaff: "",
+    emailStaff: "",
+    addressStaff: "",
+    dateBirthStaff: "",
+    carnetStaff: "",
+    photoStaff: "",
+    stateStaff: true,
+  });
+
+  const handleChange = (event) => {
+    const key = !event.key ? [event.target.name] : event.title;
+    setData({
+      ...data,
+      [key]: !event.key ? event.target.value : event.key,
+    });
+  };
+
+  const handlePhoto = async (event) => {
+    const file = event.target.files[0];
+    // const file = event.originFileObj
+    // const file = event;
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", "photos");
+    try {
+      const response = await fetch(
+        "https://api.cloudinary.com/v1_1/dqgcyonb9/image/upload",
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      if (response.ok) {
+        const { url } = await response.json();
+        setData({
+          ...data,
+          photoStaff: url,
+        });
+      } else {
+        console.error("Error al subir la imagen a Cloudinary");
+      }
+    } catch (error) {
+      console.error("Error de red:", error);
+    }
+  };
+
+  const onChangeDate = (date, dateString) => {
+    setData({
+      ...data,
+      dateBirthStaff: dateString,
+    });
+  };
+
+  const onFinish = () => {
+    if (option === "edit") {
+      dispatch(editStaff({ ...data, idStaff: idData }));
+    } else {
+      dispatch(createStaff(data));
+    }
+  };
+
+  useEffect(() => {
+    dispatch(getExtensionAll());
+    dispatch(getLevelAll());
+  }, []);
+
+  useEffect(() => {
+    idData && dispatch(getByIdStaff(idData));
+  }, [idData]);
+
+  useEffect(() => {
+    if (option === "edit" && selectIdStaff) {
+      setData({
+        idLevel: selectIdStaff.idLevel,
+        idExtension: selectIdStaff.idExtension,
+        nameStaff: selectIdStaff.nameStaff,
+        lastNameStaff: selectIdStaff.lastNameStaff,
+        emailStaff: selectIdStaff.emailStaff,
+        addressStaff: selectIdStaff.addressStaff,
+        dateBirthStaff: selectIdStaff?.dateBirthStaff.substring(0, 10),
+        carnetStaff: selectIdStaff.carnetStaff,
+        stateStaff: selectIdStaff.stateStaff,
+      });
+    }
+  }, [option, selectIdStaff]);
+
+  return (
+    <>
+      <Form
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 10,
+        }}
+        layout="horizontal"
+        style={{
+          maxWidth: 500,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item label="Nombres">
+          <Input
+            onChange={handleChange}
+            name="nameStaff"
+            placeholder="Alverto Reinaldo"
+            value={data.nameStaff}
+          />
+        </Form.Item>
+
+        <Form.Item label="Apellidos">
+          <Input
+            onChange={handleChange}
+            name="lastNameStaff"
+            placeholder="Del Rio"
+            value={data.lastNameStaff}
+          />
+        </Form.Item>
+
+        <Form.Item label="Email">
+          <Input
+            onChange={handleChange}
+            name="emailStaff"
+            placeholder="albert@gmail.com"
+            value={data.emailStaff}
+          />
+        </Form.Item>
+
+        <Form.Item label="Carnet">
+          <Input
+            onChange={handleChange}
+            name="carnetStaff"
+            placeholder="8569134"
+            value={data.carnetStaff}
+          />
+        </Form.Item>
+
+        <Form.Item label="Extension">
+          <SelectComponet
+            list={selectExtension}
+            handleChange={handleChange}
+            flag="Extension"
+            value={selectIdStaff?.department}
+          />
+        </Form.Item>
+
+        <Form.Item label="Nivel">
+          <SelectComponet
+            list={selectLevel}
+            handleChange={handleChange}
+            flag="Level"
+            value={selectIdStaff?.nameLevel}
+          />
+        </Form.Item>
+
+        <Form.Item label="Fecha de nacimiento">
+          <DatePicker
+            placeholder={"2019-09-03"}
+            name="dateBirthStaff"
+            onChange={onChangeDate}
+            value={option === "edit" && dayjs(data?.dateBirthStaff)}
+          />
+        </Form.Item>
+
+        <Form.Item label="Dirección">
+          <TextArea
+            rows={4}
+            onChange={handleChange}
+            name="addressStaff"
+            placeholder="Calle Siempre Viva N°666"
+            value={data.addressStaff}
+          />
+        </Form.Item>
+
+        {option === "edit" && (
+          <Form.Item label="Estado" valuePropName="checked">
+            <Switch />
+          </Form.Item>
+        )}
+
+        <Form.Item
+          label="Foto"
+          valuePropName="fileList"
+          getValueFromEvent={normFile}
+        >
+          {/* <Upload action="/upload.do" listType="picture-card">
+            <button
+              style={{
+                border: 0,
+                background: "none",
+              }}
+              type="button"
+            >
+              <PlusOutlined />
+              <div
+                style={{
+                  marginTop: 8,
+                }}
+              />
+            </button>
+          </Upload> */}
+          <input type="file" onChange={handlePhoto} />
+          {/* <PhotoLoading handle={handlePhoto} /> */}
+        </Form.Item>
+
+        <Button type="primary" htmlType="submit">
+          <Text text="Crear" />
+        </Button>
+      </Form>
+    </>
+  );
+}
+
+export default FormComponet;
