92014121c74178f2bbd4c8443b172298f873b92a
migayalach <ayalachavezmiguel@gmail.com>
2024-04-19 23:36:33 -0400

    maquetacion de page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -2,6 +2,41 @@ import "../stylesheet/page.css";
 
 export default function Home() {
   return (
-    <h1>Holis</h1>
+    <div className="container">
+      <nav>
+        <div className="nav_logo">
+          <a href="#">
+            <img src="#" alt="logo" />
+            LOGO
+          </a>
+        </div>
+        <ul className="nav-link">
+          <li className="link">
+            <a href="#">Home</a>
+          </li>
+          <li className="link">
+            <a href="#">Home</a>
+          </li>
+          <li className="link">
+            <a href="#">Home</a>
+          </li>
+          <li className="link">
+            <a href="#">Home</a>
+          </li>
+          <li className="link">
+            <a href="#">Home</a>
+          </li>
+        </ul>
+        <button className="btn-login">Login</button>
+      </nav>
+
+      <div className="container-body">
+        <h1>Boddy</h1>
+      </div>
+      
+      <div>
+        <h1>Footer</h1>
+      </div>
+    </div>
   );
 }
