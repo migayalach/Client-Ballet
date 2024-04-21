17b007645d9f35ac0a499a63479b3f6250b87cc4
migayalach <ayalachavezmiguel@gmail.com>
2024-04-20 23:22:27 -0400

    route hours

---
+++ client/src/app/hours/page.jsx
@@ -0,0 +1,42 @@
+"use client";
+
+// COMPONET'S
+// import NavBar from "@/components/navBar/NavBar";
+import List from "@/components/list/List";
+import FloatOption from "@/components/floatOption/FloatOption";
+
+// HOOK'S
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// LIBRARY
+
+//REDUX
+import { getHoursAll } from "@/redux/actions";
+
+// JAVASCRIP
+
+// STYLESHEET'
+
+function Hours() {
+  const dispatch = useDispatch();
+  const selectHours = useSelector((state) => state.root.hours);
+  useEffect(() => {
+    dispatch(getHoursAll());
+  }, []);
+
+  return (
+    <div>
+      <div>{/* <NavBar /> */}</div>
+      <div>
+        <h3>Lista de horarios</h3>
+        <List data={selectHours} />
+      </div>
+      <div>
+        <FloatOption />
+      </div>
+    </div>
+  );
+}
+
+export default Hours;
