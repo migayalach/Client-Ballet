6c98b43304f84b9ce7d274a393125a12fca6d5b5
migayalach <ayalachavezmiguel@gmail.com>
2024-04-23 20:59:53 -0400

    table component

--- client/src/components/tableComponent/TableComponent.jsx
+++ client/src/components/tableComponent/TableComponent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from "antd";
+import { Table, Tag } from "antd";
 import ButtonDelete from "@/components/button/buttonDelete/ButtonDelete";
 import ButtonEdit from "@/components/button/buttonEdit/ButtonEdit";
 
@@ -49,10 +49,103 @@ const dataMap = (data) => {
   }));
 };
 
+const columsClass = [
+  {
+    title: "N°",
+    dataIndex: "numberItem",
+    key: "numberItem",
+  },
+  { title: "Tipo de clase", dataIndex: "nameClass", key: "nameClass" },
+  {
+    title: "Profesor",
+    dataIndex: "teacher",
+    key: "teacher",
+  },
+  {
+    title: "Carnet",
+    dataIndex: "carnetStaff",
+    key: "carnetStaff",
+  },
+  {
+    title: "Extension",
+    dataIndex: "department",
+    key: "department",
+  },
+  { title: "Paralelo", dataIndex: "parallel", key: "parallel" },
+  {
+    title: "Estado",
+    dataIndex: "stateClass",
+    key: "stateClass",
+    render: (stateClass) => {
+      let color = stateClass ? "green" : "volcano";
+      let text = stateClass ? "Habilidato" : "Deshabilitado";
+      return <Tag color={color}>{text}</Tag>;
+    },
+  },
+  { title: "Duración", dataIndex: "totalTime", key: "totalTime" },
+  {
+    title: "Editar",
+    key: "action",
+    render: (data) => (
+      <ButtonEdit idData={data.idClass} text="Editar" render="CLASS" />
+    ),
+  },
+  {
+    title: "Eliminar",
+    key: "action",
+    render: (data) => (
+      <ButtonDelete idData={data.idClass} text="Eliminar" render="CLASS" />
+    ),
+  },
+];
+
+const classMap = (data) => {
+  return data?.map(
+    (
+      {
+        idClass,
+        nameClass,
+        nameStaff,
+        lastNameStaff,
+        carnetStaff,
+        department,
+        parallel,
+        stateClass,
+        totalTime,
+      },
+      index
+    ) => ({
+      key: index,
+      idClass,
+      numberItem: index + 1,
+      nameClass,
+      teacher: `${nameStaff} ${lastNameStaff}`,
+      carnetStaff,
+      department,
+      parallel,
+      stateClass,
+      totalTime: `${totalTime} hrs`,
+    })
+  );
+};
+
 function TableComponent({ data, render }) {
   return (
     <div>
-      <Table columns={columns} dataSource={dataMap(data)} pagination={false} />
+      {render === "TYPE-CLASS" && (
+        <Table
+          columns={columns}
+          dataSource={dataMap(data)}
+          pagination={false}
+        />
+      )}
+      {render === "CLASS" && (
+        <Table
+          columns={columsClass}
+          dataSource={classMap(data)}
+          pagination={false}
+        />
+      )}
     </div>
   );
 }
