df8a42629b94c20fc89248a9b47d8e9c622e953a
migayalach <ayalachavezmiguel@gmail.com>
2024-05-15 23:44:20 -0400

    card component

--- client/src/components/card/CardComponent.jsx
+++ client/src/components/card/CardComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { Card } from "antd";
 const { Meta } = Card;
@@ -7,38 +7,56 @@ import "./card-component.css";
 import ButtonDelete from "../button/buttonDelete/ButtonDelete";
 import ButtonRenderId from "../button/buttonRenderId/ButtonRenderId";
 
-function CardComponent({ user }) {
+function CardComponent({
+  idUser,
+  idLevel,
+  idExtension,
+  nameUser,
+  lastNameUser,
+  emailUser,
+  addressUser,
+  dateBirthUser,
+  carnetUser,
+  photoUser,
+  stateUser,
+}) {
+  const [dataUser, setDataUser] = useState({
+    idUser,
+    idLevel,
+    idExtension,
+    nameUser,
+    lastNameUser,
+    emailUser,
+    addressUser,
+    dateBirthUser,
+    carnetUser,
+    photoUser,
+    stateUser,
+  });
+
   return (
-    <div className="container">
-      {user?.map(
-        ({ idUser, nameUser, lastNameUser, carnetUser, photoUser }, index) => (
-          <div key={index}>
-            <Card
-              hoverable
-              style={{
-                width: 240,
-              }}
-              cover={
-                <img alt="example" src={photoUser} className="image-photo" />
-              }
-              actions={[
-                <Link href={`/user/${idUser}`}>
-                  <ButtonRenderId />
-                </Link>,
-                <ButtonEdit idData={idUser} text="editar" render="USER" />,
-                <ButtonDelete idData={idUser} text="eliminar" render="USER" />,
-              ]}
-              className="card"
-            >
-              <Meta
-                title={`${nameUser} ${lastNameUser}`}
-                description={`Carnet: ${carnetUser}`}
-                className="card-data"
-              />
-            </Card>
-          </div>
-        )
-      )}
+    <div>
+      <Card
+        hoverable
+        style={{
+          width: 240,
+        }}
+        cover={<img alt="example" src={photoUser} className="image-photo" />}
+        actions={[
+          <Link href={`/user/${idUser}`}>
+            <ButtonRenderId />
+          </Link>,
+          <ButtonEdit dataUser={dataUser} text="editar" render="USER" />,
+          <ButtonDelete idData={idUser} text="eliminar" render="USER" />,
+        ]}
+        className="card"
+      >
+        <Meta
+          title={`${nameUser} ${lastNameUser}`}
+          description={`Carnet: ${carnetUser}`}
+          className="card-data"
+        />
+      </Card>
     </div>
   );
 }
