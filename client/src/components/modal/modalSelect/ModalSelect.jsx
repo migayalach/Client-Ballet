e78124a143219151a9811f854f16f07ac9ac96e8
migayalach <ayalachavezmiguel@gmail.com>
2024-05-28 23:33:59 -0400

    modal select

--- client/src/components/modal/modalSelect/ModalSelect.jsx
+++ client/src/components/modal/modalSelect/ModalSelect.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Button, Modal } from "antd";
 import ListData from "@/components/listData/ListData";
 import { useDispatch, useSelector } from "react-redux";
+import { filterAll, clearFilterAll } from "@/redux/actions";
 
 function ModalSelect({ render }) {
   const dispatch = useDispatch();
@@ -11,19 +12,23 @@ function ModalSelect({ render }) {
   };
   const handleOk = () => {
     setIsModalOpen(false);
+    dispatch(clearFilterAll());
   };
   const handleCancel = () => {
     setIsModalOpen(false);
+    dispatch(clearFilterAll());
   };
 
   useEffect(() => {
-    if (render === "TEACHER-ALL") {
-      dispatch();
+    if (isModalOpen) {
+      if (render === "TEACHER-ALL") {
+        dispatch(filterAll("all=teacher"));
+      }
+      if (render === "TYPE-CLASS-ALL") {
+        dispatch(filterAll("all=class"));
+      }
     }
-    if (render === "TYPE-CLASS-ALL") {
-      dispatch();
-    }
-  }, [render]);
+  }, [isModalOpen]);
 
   return (
     <>
