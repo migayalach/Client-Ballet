a00995c2e1237b09e886412d3d7d2809a46bed40
migayalach <ayalachavezmiguel@gmail.com>
2024-06-02 23:48:14 -0400

    float option

--- client/src/components/floatOption/FloatOption.jsx
+++ client/src/components/floatOption/FloatOption.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import InfoModal from "../modal/infoModal/InfoModal";
-import { QuestionCircleOutlined, FileAddOutlined } from "@ant-design/icons";
-import { FloatButton } from "antd";
 import CreateModal from "../modal/createModal/CreateModal";
+import QualificationModal from "../modal/qualificationModal/QualificationModal";
+import {
+  QuestionCircleOutlined,
+  FileAddOutlined,
+  CopyOutlined,
+} from "@ant-design/icons";
+import { FloatButton } from "antd";
 
 function FloatOption({ render, idClass }) {
   const [flagAdd, setFlagAdd] = useState(false);
   const [flagInfo, setFlagInfo] = useState(false);
+  const [flagQualification, setFlagQualification] = useState(false);
 
   const handleAdd = () => {
     setFlagAdd(!flagAdd);
@@ -16,28 +22,58 @@ function FloatOption({ render, idClass }) {
     setFlagInfo(!flagInfo);
   };
 
+  const handleQualification = () => {
+    setFlagQualification(!flagQualification);
+  };
+
   return (
     <div className="conteiner">
       <FloatButton
         icon={<FileAddOutlined />}
+        tooltip={<div>Nuevo</div>}
         type="primary"
         style={{
-          right: 24,
+          right: 10,
+          bottom: 20,
         }}
         onClick={() => handleAdd()}
       />
 
       <FloatButton
         icon={<QuestionCircleOutlined />}
+        tooltip={<div>Información</div>}
         type="primary"
         style={{
-          right: 94,
+          right: 80,
+          bottom: 20,
         }}
         onClick={() => handleInfo()}
       />
+
+      {idClass > 0 && (
+        <FloatButton
+          icon={<CopyOutlined />}
+          tooltip={<div>Nueva calificación</div>}
+          type="primary"
+          style={{
+            right: 150,
+            bottom: 20,
+          }}
+          onClick={() => handleQualification()}
+        />
+      )}
+
+      {flagQualification && (
+        <QualificationModal
+          flag={true}
+          handleQualification={handleQualification}
+        />
+      )}
+
       {flagInfo && (
         <InfoModal flag={true} handleInfo={handleInfo} render={render} />
       )}
+
       {flagAdd && (
         <CreateModal
           flag={true}
