129aea3774b097dcabe6453821ec1018edaa08dc
migayalach <ayalachavezmiguel@gmail.com>
2024-07-29 10:53:21 -0400

    loading

--- client/src/components/pageResult/Loading.jsx
+++ client/src/components/pageResult/Loading.jsx
@@ -4,12 +4,13 @@ import { Result } from "antd";
 function Loading() {
   return (
     <>
-      <Result status="404" title="404" subTitle="Cargando." />
+      {/* <Result status="404" title="404" subTitle="Cargando." />
       <img
         src="https://res.cloudinary.com/dqgcyonb9/image/upload/v1716768885/Ballet/nb3efcckb7infy3ebust.gif"
         alt="Reloj de arena"
         height="120px"
-      />
+      /> */}
+      <h1>HOLISSSS ESTOY CARGANDO</h1>
     </>
   );
 }
