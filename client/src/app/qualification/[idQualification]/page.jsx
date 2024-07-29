d4c7f569bc2d5f6320ae3f3ab5911a334a54c9e9
migayalach <ayalachavezmiguel@gmail.com>
2024-07-29 10:57:42 -0400

    qualification page

--- client/src/app/qualification/[idQualification]/page.jsx
+++ client/src/app/qualification/[idQualification]/page.jsx
@@ -1,18 +1,38 @@
 "use client";
 
-import React, { useEffect } from "react";
+// COMPONET'S
 import TableNotes from "@/components/tableComponent/TableNotes";
+import Page404 from "@/components/pageResult/Page404";
+
+// HOOK'S
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+
+// LIBRARY
+
+//REDUX
 import { getQualificationAll } from "@/redux/actions";
+
+// JAVASCRIP
+
+// STYLESHEET'
 
 function page({ params }) {
   const dispatch = useDispatch();
-  const {
-    dataUser: { idUser },
-  } = useSelector(({ root }) => root?.access);
+  const selectUser = useSelector(({ root }) => root?.access);
+
+  if (Object.keys(selectUser).length === 0) {
+    return (
+      <div>
+        <Page404 />
+      </div>
+    );
+  }
 
   useEffect(() => {
-    dispatch(getQualificationAll(params.idQualification, idUser));
+    dispatch(
+      getQualificationAll(params?.idQualification, selectUser?.dataUser?.idUser)
+    );
   }, []);
 
   return (
