f57a91cbcf2763efa020973ac38d99fca026ecbf
migayalach <ayalachavezmiguel@gmail.com>
2024-07-29 10:57:04 -0400

    user id page

--- client/src/app/user/[idUser]/page.jsx
+++ client/src/app/user/[idUser]/page.jsx
@@ -4,14 +4,24 @@ import { useSelector, useDispatch } from "react-redux";
 import { getByIdUser } from "@/redux/actions";
 import CollapseData from "@/components/collapseData/CollapseData";
 import "./info-staff.css";
+import Page404 from "@/components/pageResult/Page404";
 
 function InfoUser({ params }) {
   const dispatch = useDispatch();
   const selectUser = useSelector(({ root }) => root?.data);
+  const selectAccess = useSelector(({ root }) => root?.access);
 
   useEffect(() => {
     params.idUser && dispatch(getByIdUser(params.idUser));
   }, [params]);
+
+  if (Object.keys(selectAccess).length === 0) {
+    return (
+      <div>
+        <Page404 />
+      </div>
+    );
+  }
 
   return (
     <div className="conteiner-data">
