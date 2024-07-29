aa5ec8a75374378c7ebf002f1522fe1d9176ab53
migayalach <ayalachavezmiguel@gmail.com>
2024-07-29 10:53:07 -0400

    page 404

--- client/src/components/pageResult/Page404.jsx
+++ client/src/components/pageResult/Page404.jsx
@@ -1,13 +1,22 @@
-import React from "react";
-import { Button, Result } from "antd";
+import React, { useEffect } from "react";
+import { Result } from "antd";
+import { useRouter } from "next/navigation";
 
 function Page404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <Result
       status="404"
       title="404"
-      subTitle="Sorry, the page you visited does not exist."
-      extra={<Button type="primary">Back Home</Button>}
+      subTitle="Lo sientimos mucho la pagina que busca no existe!"
     />
   );
 }
