31ce4e1fea47caaa181122b4f905fd3d44956943
migayalach <ayalachavezmiguel@gmail.com>
2024-08-07 22:54:11 -0400

    Añadido: src/app/class/assistance/

---
+++ client/src/app/class/assistance/[idAssistance]/page.jsx
@@ -0,0 +1,27 @@
+"use client";
+import TableAssistance from "@/components/tableComponent/TableAssistance";
+import React, { useEffect, useState } from "react";
+import { getListIdAttendance } from "@/redux/actions";
+import { useSelector, useDispatch } from "react-redux";
+
+function page({ params }) {
+  const dispatch = useDispatch();
+  const selectAttendanceList = useSelector(({ root }) => root?.attendance);
+
+  useEffect(() => {
+    dispatch(getListIdAttendance(params?.idAssistance));
+
+    return () => {
+      // console.log(":D");
+    };
+  }, []);
+
+  return (
+    <div>
+      LISTA DE CLASE A CALIFICAR - {params.idAssistance}{" "}
+      <TableAssistance list={selectAttendanceList} />
+    </div>
+  );
+}
+
+export default page;
