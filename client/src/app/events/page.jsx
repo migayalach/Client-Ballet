5271790f9d859cd6c326e7fa91bc6eee17760dbd
migayalach <ayalachavezmiguel@gmail.com>
2024-08-15 22:18:54 -0400

    page events

--- client/src/app/events/page.jsx
+++ client/src/app/events/page.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getListEventsAll } from "@/redux/actions";
+import ListContainer from "@/components/home/listEvents/listContainer/ListContainer";
 
 function Events() {
+  const dispatch = useDispatch();
+  const selectList = useSelector(({ root }) => root?.list);
+
+  useEffect(() => {
+    dispatch(getListEventsAll());
+    return () => {
+      // console.log(";D");
+    };
+  }, []);
+
   return (
     <div>
-      
+      holis
+      <ListContainer list={selectList} />
+      <div>Paginado</div>
+      <div>Agregar nuevos</div>
     </div>
-  )
+  );
 }
 
-export default Events
+export default Events;
