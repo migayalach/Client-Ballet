bd7801f55ef3e5d0a656f8d32b27de26b77e7925
migayalach <ayalachavezmiguel@gmail.com>
2024-08-31 22:35:14 -0400

    send page

--- client/src/app/send/page.jsx
+++ client/src/app/send/page.jsx
@@ -3,10 +3,13 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import TableComponent from "@/components/tableComponent/TableComponent";
 import { getContactAll } from "@/redux/actions";
+import SendFilter from "@/components/filters/sendFilter/SendFilter";
+import PaginationComponet from "@/components/pagination/PaginationComponet";
 
 function page() {
   const dispatch = useDispatch();
   const selectList = useSelector(({ root }) => root?.contact);
+  const selectInfo = useSelector((state) => state?.root?.info);
 
   useEffect(() => {
     dispatch(getContactAll());
@@ -14,13 +17,13 @@ function page() {
 
   return (
     <div>
-      <p>filtros</p>
+      <SendFilter />
       <TableComponent
         data={selectList}
         render="CONTACT"
         /*access={}*/
       />
-      <p>PAGINACION</p>
+      <PaginationComponet pages={selectInfo?.pages} navegation="CONTACT" />
     </div>
   );
 }
