ea410d37729ffaba529e22eb31382203c2a1d5d4
migayalach <ayalachavezmiguel@gmail.com>
2024-09-21 23:27:14 -0400

    events page

--- client/src/app/events/page.jsx
+++ client/src/app/events/page.jsx
@@ -5,17 +5,28 @@ import { getListEventsAll } from "@/redux/actions";
 import ListContainer from "@/components/home/listEvents/listContainer/ListContainer";
 import FloatOption from "@/components/floatOption/FloatOption";
 import PaginationComponet from "@/components/pagination/PaginationComponet";
+import Loading from "@/components/pageResult/Loading";
 
 function Events() {
   const dispatch = useDispatch();
   const selectList = useSelector(({ root }) => root?.list);
+  const selectInfo = useSelector(({ root }) => root?.info);
+  const selectFilter = useSelector((state) => state.root?.filter);
+  const selectAccess = useSelector((state) => state.root?.access);
 
   useEffect(() => {
-    dispatch(getListEventsAll());
-    return () => {
-      // console.log(";D");
-    };
+    if (!selectFilter.length) {
+      dispatch(getListEventsAll());
+    }
   }, []);
+
+  if (!selectList?.length && !selectInfo) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -25,15 +36,14 @@ function Events() {
         <ListContainer list={selectList} />
       </div>
 
-      <div>Paginado</div>
+      <div>
+        <PaginationComponet pages={selectInfo?.pages} navegation="EVENTS" />
+      </div>
 
       <div>
-        <FloatOption
-          render="EVENTS"
-          access="Director"
-          // access={selectAccess?.level}
-        />
+        <FloatOption render="EVENTS" access={selectAccess?.level} />
       </div>
+      
     </div>
   );
 }
