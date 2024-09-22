76e478ef42babe260fe957366320a46e7c6ff9fc
migayalach <ayalachavezmiguel@gmail.com>
2024-09-21 23:26:57 -0400

    page idEvent

--- client/src/app/events/[idEvent]/page.jsx
+++ client/src/app/events/[idEvent]/page.jsx
@@ -1,12 +1,14 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getEventId } from "@/redux/actions";
+import { getEventId, stateClear } from "@/redux/actions";
 import FloatOption from "@/components/floatOption/FloatOption";
 
 function page({ params }) {
   const dispatch = useDispatch();
+  const selectState = useSelector(({ root }) => root?.state);
   const selectEvent = useSelector(({ root }) => root?.data);
+  const selectAccess = useSelector((state) => state.root?.access);
 
   const [data, setData] = useState({
     idListEvent: "",
@@ -17,6 +19,14 @@ function page({ params }) {
     stateEvent: false,
     urlPicture: "",
   });
+
+  useEffect(() => {
+    selectState === "edit"
+      ? setTimeout(() => {
+          dispatch(stateClear());
+        }, 500)
+      : "";
+  }, [selectState]);
 
   useEffect(() => {
     dispatch(getEventId(params?.idEvent));
@@ -57,17 +67,33 @@ function page({ params }) {
     return () => {
       // console.log(";D");
     };
-  }, [selectEvent]);
-
-  console.log(data);
+  }, [selectEvent, selectState]);
 
   return (
     <div>
-      <div></div>
+      <div>
+        <p>{data.hourEvent}</p>
+        <p>{data.dateNews}</p>
+        <p>{data.title}</p>
+        <p>{data.body}</p>
+        <p>{data.stateEvent}</p>
+      </div>
+
       <div>
         Here be the photo almacening in the state local of component Data.
+        <img src={data.urlPicture} height={480} width={500}></img>
       </div>
-      <FloatOption />
+
+      {(selectAccess?.level === "Director" ||
+        selectAccess?.level === "Secretaria") && (
+        <div>
+          <FloatOption
+            render="EVENTS"
+            access={selectAccess?.level}
+            event="edit"
+          />
+        </div>
+      )}
     </div>
   );
 }
