e28fa245b8b76405307497c94ce16443a3a669b0
migayalach <ayalachavezmiguel@gmail.com>
2024-09-22 22:40:48 -0400

    list students

--- client/src/components/listStudents/ListStudents.jsx
+++ client/src/components/listStudents/ListStudents.jsx
@@ -2,28 +2,18 @@ import React, { useState, useEffect } from "react";
 import { Avatar, List } from "antd";
 import { getIdAllClassStudent } from "@/redux/actions";
 import { useDispatch, useSelector } from "react-redux";
+import TableComponent from "../tableComponent/TableComponent";
 
-function ListStudents({ idClass }) {
+function ListStudents({ idClass, access }) {
   const dispatch = useDispatch();
   const selectClassStudent = useSelector(({ root }) => root?.student);
-  const data = selectClassStudent.map(
-    ({
-      idUser,
-      idClass,
-      nameUser,
-      lastNameUser,
-      carnetUser,
-      department,
-      photoUser,
-      stateUser,
-    }) => ({
-      idUser,
-      idClass,
-      title: `${nameUser} ${lastNameUser} ${carnetUser} ${department}`,
-      photoUser,
-      stateUser,
-    })
-  );
+  // const selectInfo = useSelector((state) => state.root.info);
+  // const selectFilter = useSelector((state) => state.root?.filter);
+  // const selectAccess = useSelector(({ root }) => root?.access);
+
+  const handleDelete = (idClass, idUser) => {
+    alert(`${idClass}, ${idUser}`);
+  };
 
   useEffect(() => {
     dispatch(getIdAllClassStudent(idClass));
@@ -31,23 +21,11 @@ function ListStudents({ idClass }) {
 
   return (
     <>
-      <List
-        pagination={false}
-        dataSource={data}
-        renderItem={(item, index) => (
-          <List.Item
-            actions={[
-              <a key="list-loadmore-more">estado{item.idUser}</a>,
-              <a key="list-loadmore-edit">calificaciones</a>,
-            ]}
-          >
-            <List.Item.Meta
-              avatar={<Avatar src={`${item.photoUser}`} />}
-              title={<a href="https://ant.design">{item.title}</a>}
-              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-            />
-          </List.Item>
-        )}
+      <TableComponent
+        data={selectClassStudent.length ? selectClassStudent : selectFilter}
+        render="LIST-STUDENT-CLASS"
+        access={access}
+        handleDelete={handleDelete}
       />
     </>
   );
