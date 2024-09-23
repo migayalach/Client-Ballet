27174948127430555e5d3f31333888abfc4f4113
migayalach <ayalachavezmiguel@gmail.com>
2024-09-22 22:40:07 -0400

    class idClass page

--- client/src/app/class/[idClass]/page.jsx
+++ client/src/app/class/[idClass]/page.jsx
@@ -3,39 +3,62 @@
 // COMPONET'S
 import FloatOption from "@/components/floatOption/FloatOption";
 import Page404 from "@/components/pageResult/Page404";
+import TableComponent from "@/components/tableComponent/TableComponent";
 
 // HOOK'S
-import React from "react";
-import { useSelector } from "react-redux";
-import ListStudents from "@/components/listStudents/ListStudents";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 //REDUX
+import { getIdAllClassStudent } from "@/redux/actions";
 
 // STYLESHEET'
 
 function ClassParams({ params }) {
-  const selectUser = useSelector(({ root }) => root?.access);
+  const dispatch = useDispatch();
+  const selectClassStudent = useSelector(({ root }) => root?.student);
+  const selectInfo = useSelector((state) => state.root.info);
+  const selectFilter = useSelector((state) => state.root?.filter);
+  const selectAccess = useSelector(({ root }) => root?.access);
 
-  if (Object.keys(selectUser).length === 0) {
-    return (
-      <div>
-        <Page404 />
-      </div>
-    );
-  }
+  // const selectUser = useSelector(({ root }) => root?.access);
+  // if (Object.keys(selectUser).length === 0) {
+  //   return (
+  //     <div>
+  //       <Page404 />
+  //     </div>
+  //   );
+  // }
+
+  const handleDelete = (idClass, idUser) => {
+    alert(`${idClass}, ${idUser}`);
+  };
+
+  useEffect(() => {
+    dispatch(getIdAllClassStudent(params?.idClass));
+  }, []);
 
   return (
     <div>
-      <h1>ESTUDIANTES</h1>
-      <h2>{params.idClass}</h2>
+      <h3>Lista de estudiantes</h3>
+
+      <div>FILTROS</div>
+
       <div>
-        <ListStudents idClass={params.idClass} />
+        <TableComponent
+          data={selectClassStudent.length ? selectClassStudent : selectFilter}
+          render="LIST-STUDENT-CLASS"
+          access={selectAccess?.level}
+          handleDelete={handleDelete}
+        />
       </div>
+
+      <div>PAGINADO</div>
       <div>
         <FloatOption
           render="CLASS-STUDENT"
           idClass={params.idClass}
-          access={selectUser?.level}
+          access={selectAccess?.level}
         />
       </div>
     </div>
