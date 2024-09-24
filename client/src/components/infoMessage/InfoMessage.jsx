dd033c8a38dc181ab1027a46f9b6bdd92a5cf53d
migayalach <ayalachavezmiguel@gmail.com>
2024-09-24 10:58:18 -0400

    info message

--- client/src/components/infoMessage/InfoMessage.jsx
+++ client/src/components/infoMessage/InfoMessage.jsx
@@ -1,37 +1,41 @@
 import React from "react";
 import { Alert, Space } from "antd";
+import Notification from "../modal/notification/Notification";
 
 function InfoMessage({ dataState: { state, message }, clearLocalState }) {
   console.log(state);
-  
+
   return (
-    <Space
-      direction="vertical"
-      style={{
-        width: "100%",
-      }}
-    >
-      {/* POSIBLES 4 ESTADOS
-    create
-    delete
-    update
-    error */}
-
-      {/* {(message === "No se puede agregar numeros negativos" && (
-        <Alert message={message} type="error" showIcon />
-      )) ||
-        (message === "Se sumo correctamente...!" && (
-          <Alert message={message} type="success" showIcon />
-        )) ||
-        (message === "Se resto correctamente...!" && (
-          <Alert message={message} type="info" showIcon />
-        ))} */}
-      {state === "create" && (
-        <Alert message={`Creado ${message}`} type="success" showIcon />
-      )}
-
-    </Space>
+    <>
+      <Notification />
+    </>
   );
 }
 
 export default InfoMessage;
+
+//   {/* POSIBLES 4 ESTADOS
+// create
+// delete
+// update
+// error */}
+
+//   {/* {(message === "No se puede agregar numeros negativos" && (
+//     <Alert message={message} type="error" showIcon />
+//   )) ||
+//     (message === "Se sumo correctamente...!" && (
+//       <Alert message={message} type="success" showIcon />
+//     )) ||
+//     (message === "Se resto correctamente...!" && (
+//       <Alert message={message} type="info" showIcon />
+//     ))} */}
+//     {state === "create" && (
+//       <Alert message={`Creado ${message}`} type="success" showIcon />
+//     )}
+//     {state === "error" && (
+//       <Alert
+//         message={`No se puedo realizar esta operacion!`}
+//         type="error"
+//         showIcon
+//       />
+//     )}
