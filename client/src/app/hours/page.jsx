655323be250d3ad2e0a8b6d77e77ce8ebb8c2019
migayalach <ayalachavezmiguel@gmail.com>
2024-09-27 06:06:55 -0400

    hours page

--- client/src/app/hours/page.jsx
+++ client/src/app/hours/page.jsx
@@ -7,32 +7,64 @@ import Loading from "@/components/pageResult/Loading";
 import Page404 from "@/components/pageResult/Page404";
 import TableComponent from "@/components/tableComponent/TableComponent";
 import HoursFilter from "@/components/filters/hoursFilter/HoursFilter";
+import Notification from "@/components/modal/notification/Notification";
 
 // HOOK'S
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 // LIBRARY
 
 //REDUX
-import { getHoursAll } from "@/redux/actions";
-
-// JAVASCRIP
+import { getHoursAll, hoursClear } from "@/redux/actions";
 
 // STYLESHEET'
 
+// JAVASCRIP
+
 function Hours() {
   const dispatch = useDispatch();
+  const [dataState, setDataState] = useState({ state: null, message: "" });
+  const [flagAlert, setFlagAlert] = useState(false);
   const selectHours = useSelector((state) => state.root.hours);
   const selectInfo = useSelector((state) => state.root.info);
   const selectFilter = useSelector((state) => state.root?.filter);
   const selectAccess = useSelector((state) => state.root?.access);
+  const selectState = useSelector(({ root }) => root?.state);
+  const selectError = useSelector(({ root }) => root?.error);
+
+  const clearLocalState = () => {
+    setDataState({
+      state: null,
+      message: "",
+    });
+    setFlagAlert(false);
+  };
 
   useEffect(() => {
     if (!selectFilter.length) {
       dispatch(getHoursAll());
     }
+    return () => {
+      dispatch(hoursClear());
+    };
   }, []);
+
+  useEffect(() => {
+    if (selectState?.length) {
+      setDataState({
+        state: selectState,
+        message: "con exito",
+      });
+    } else if (selectError !== null) {
+      setDataState({
+        state: "error",
+        message: selectError?.error,
+      });
+    }
+    setFlagAlert(true);
+    return () => {};
+  }, [selectState, selectError]);
 
   if (Object.keys(selectAccess).length === 0) {
     return (
@@ -59,6 +91,7 @@ function Hours() {
           <HoursFilter />
         </div>
       )}
+
       <div>
         <h3>Lista de horarios</h3>
         <TableComponent
@@ -67,13 +100,24 @@ function Hours() {
           access={selectAccess?.level}
         />
       </div>
+
       <div>
         {selectInfo && (
           <PaginationComponet pages={selectInfo.pages} navegation="HOURS" />
         )}
       </div>
+
       <div>
         <FloatOption render="HOURS" access={selectAccess?.level} />
+      </div>
+
+      <div>
+        {flagAlert && (
+          <Notification
+            dataState={dataState}
+            clearLocalState={clearLocalState}
+          />
+        )}
       </div>
     </div>
   );
