300d7bfab68510867b188e235a51ef9d2b26f23b
migayalach <ayalachavezmiguel@gmail.com>
2024-09-28 11:06:47 -0400

    form validation user

---
+++ client/src/components/form/formUser/formValidationUser.js
@@ -0,0 +1,22 @@
+const formValidationUser = (data) => {
+  const errors = {};
+
+  if (data.idExtension) {
+  }
+
+  if (data.nameUser) {
+  }
+
+  if (data.lastNameUser) {
+  }
+
+  if (data.emailUser) {
+  }
+
+  if (data.carnetUser) {
+  }
+
+  return errors;
+};
+
+export default formValidationUser;
