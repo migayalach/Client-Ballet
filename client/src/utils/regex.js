86575b0642a0f7860ebc6f0c6a4ef995aa5ae87c
migayalach <ayalachavezmiguel@gmail.com>
2024-09-28 11:07:29 -0400

    utils regex

--- client/src/utils/regex.js
+++ client/src/utils/regex.js
@@ -1,2 +1,8 @@
 export const emailRegex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 export const numberContact = /^\d+$/;
+export const passwordRegexMay_AZ = /(?=.*[A-Z])/;
+export const passwordRegexMin_az = /(?=.*[a-z])/;
+export const passwordRegexDigits = /(?=.*\d)/;
+export const passwordRegexSpecialCaracter = /(?=.*[@#$%^&+=.!_])/;
+export const passwordRegexLength = /^.{8,}$/;
+export const passwordRegex = /.*\d+.*/;
