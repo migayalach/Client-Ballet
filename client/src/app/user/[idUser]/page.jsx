c14064a88f8c6caab5645637fb6269b685452872
migayalach <ayalachavezmiguel@gmail.com>
2024-10-02 20:51:12 -0400

    user [idUser] page

--- client/src/app/user/[idUser]/page.jsx
+++ client/src/app/user/[idUser]/page.jsx
@@ -1,16 +1,17 @@
 "use client";
 // COMPONET'S
-import CollapseData from "@/components/collapseData/CollapseData";
 import Page404 from "@/components/pageResult/Page404";
+import FloatOption from "@/components/floatOption/FloatOption";
+import Notification from "@/components/modal/notification/Notification";
 
 // HOOK'S
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 // LIBRARY
 
 //REDUX
-import { getByIdUser } from "@/redux/actions";
+import { stateClear, getByIdUser } from "@/redux/actions";
 
 // JAVASCRIP
 
@@ -19,12 +20,14 @@ import "./info-staff.css";
 
 function InfoUser({ params }) {
   const dispatch = useDispatch();
-  const selectUser = useSelector(({ root }) => root?.data);
+  const [data, setData] = useState({});
+  const [dataState, setDataState] = useState({ state: null, message: "" });
+  const [flagAlert, setFlagAlert] = useState(false);
+  const selectDataUser = useSelector(({ root }) => root?.data);
+  const selectFilter = useSelector((state) => state.root?.filter);
   const selectAccess = useSelector(({ root }) => root?.access);
-
-  useEffect(() => {
-    params.idUser && dispatch(getByIdUser(params.idUser));
-  }, [params]);
+  const selectState = useSelector(({ root }) => root?.state);
+  const selectError = useSelector(({ root }) => root?.error);
 
   if (Object.keys(selectAccess).length === 0) {
     return (
@@ -34,16 +37,69 @@ function InfoUser({ params }) {
     );
   }
 
+  const clearLocalState = () => {
+    setDataState({
+      state: null,
+      message: "",
+    });
+    setFlagAlert(false);
+  };
+
+  useEffect(() => {
+    if (selectState === "edit") {
+      setDataState({
+        state: selectState,
+        message: `Editado con exito`,
+      });
+    } else if (selectError !== null) {
+      setDataState({
+        state: "error",
+        message: selectError?.error,
+      });
+    }
+    setFlagAlert(true);
+    return () => {};
+  }, [selectState, selectError]);
+
+  useEffect(() => {
+    if (selectState === "edit") {
+      setTimeout(() => {
+        dispatch(stateClear());
+      }, 1500);
+    }
+  }, [selectState, selectDataUser]);
+
+  useEffect(() => {
+    dispatch(getByIdUser(params.idUser));
+  }, []);
+
+  useEffect(() => {
+    setData({ ...selectDataUser });
+  }, [selectDataUser]);
+
   return (
     <div className="conteiner-data">
       <div>
-        <h1>{selectUser?.nameUser}</h1>
-        <h1>{selectUser?.nameLevel}</h1>
-        <h1>{selectUser?.carnetUser}</h1>
-        <h1>{selectUser?.department}</h1>
+        <h1>{data?.nameUser}</h1>
+        filtros para dos casos: estudiante = Si es estudiante muestra las clases
+        que pasa docente = Si es docente filtra y muestra sus cursos impartidos
+        con opcines a imprimir
       </div>
       <div>
-        <CollapseData render="user" />
+        <FloatOption
+          idUser={params.idUser}
+          render="USER"
+          access={selectAccess?.level}
+          event="edit"
+        />
+      </div>
+      <div>
+        {flagAlert && (
+          <Notification
+            dataState={dataState}
+            clearLocalState={clearLocalState}
+          />
+        )}
       </div>
     </div>
   );
