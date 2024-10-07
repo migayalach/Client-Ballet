4b026f96e62a0a905e26f5685fc9db4260c6fbcd
migayalach <ayalachavezmiguel@gmail.com>
2024-10-06 21:57:30 -0400

    notification

--- client/src/components/modal/notification/Notification.jsx
+++ client/src/components/modal/notification/Notification.jsx
@@ -4,77 +4,88 @@ import { Modal } from "antd";
 import { removeError, stateClear, removeAux } from "@/redux/actions";
 
 const Notification = ({ dataState: { state, message }, clearLocalState }) => {
-  const dispatch = useDispatch();
-  const [modal, contextHolder] = Modal.useModal();
-  const [isNotified, setIsNotified] = useState(false);
+  if (
+    (state === null && !message.length) ||
+    state === "login" ||
+    (state === "error" && message === "Contraseña incorrecta")
+  ) {
+    const dispatch = useDispatch();
+    const [modal, contextHolder] = Modal.useModal();
+    const [isNotified, setIsNotified] = useState(false);
 
-  const countDown = () => {
-    if (isNotified) return;
-    setIsNotified(true);
+    const countDown = () => {
+      if (isNotified) return;
+      setIsNotified(true);
 
-    let secondsToGo = 3;
-    let instance;
+      let secondsToGo = 3;
+      let instance;
 
-    if (state === "success") {
-      instance = modal.success({
-        title: "Operación exitosa",
-        content: `${message}`,
-      });
-      dispatch(stateClear());
-      dispatch(removeAux());
-    } else if (state === "login") {
-      instance = modal.success({
-        title: "Operación exitosa",
-        content: `${message}`,
-      });
-      dispatch(stateClear());
-    } else if (state === true) {
-      instance = modal.info({
-        title: "Operación exitosa",
-        content: `${message}`,
-      });
-    } else if (state === "error") {
-      instance = modal.error({
-        title: "Operación interrumpida",
-        content: `${message}`,
-      });
-    } else if (state === "create") {
-      instance = modal.success({
-        title: "Operación realizada con éxito",
-        content: `Creado con éxito!`,
-      });
-    } else if (state === "delete") {
-      instance = modal.info({
-        title: "Operación realizada con éxito",
-        content: `Eliminado con éxito!`,
-      });
-    } else if (state === "edit") {
-      instance = modal.warning({
-        title: "Operación realizada con éxito",
-        content: `Actualización realizada con éxito!`,
-      });
-    }
+      if (state === "success") {
+        instance = modal.success({
+          title: "Operación exitosa",
+          content: `${message}`,
+        });
+        dispatch(stateClear());
+        dispatch(removeAux());
+      } else if (state === "login") {
+        instance = modal.success({
+          title: "Operación exitosa",
+          content: `${message}`,
+        });
+        dispatch(stateClear());
+      } else if (state === true) {
+        instance = modal.info({
+          title: "Operación exitosa",
+          content: `${message}`,
+        });
+      } else if (state === "error-change") {
+        instance = modal.error({
+          title: "Operación interrumpida",
+          content: `${message}`,
+        });
+      } else if (state === "error") {
+        instance = modal.error({
+          title: "Operación interrumpida",
+          content: `${message}`,
+        });
+      } else if (state === "create") {
+        instance = modal.success({
+          title: "Operación realizada con éxito",
+          content: `Creado con éxito!`,
+        });
+      } else if (state === "delete") {
+        instance = modal.info({
+          title: "Operación realizada con éxito",
+          content: `Eliminado con éxito!`,
+        });
+      } else if (state === "edit") {
+        instance = modal.warning({
+          title: "Operación realizada con éxito",
+          content: `Actualización realizada con éxito!`,
+        });
+      }
 
-    const timer = setInterval(() => {
-      secondsToGo -= 1;
-    }, 1000);
+      const timer = setInterval(() => {
+        secondsToGo -= 1;
+      }, 1000);
 
-    setTimeout(() => {
-      clearInterval(timer);
-      instance.destroy();
-      clearLocalState();
-      dispatch(removeError());
-      setIsNotified(false);
-    }, secondsToGo * 1000);
-  };
+      setTimeout(() => {
+        clearInterval(timer);
+        instance.destroy();
+        clearLocalState();
+        dispatch(removeError());
+        setIsNotified(false);
+      }, secondsToGo * 1000);
+    };
 
-  useEffect(() => {
-    if (state && !isNotified) {
-      countDown();
-    }
-  }, [state, message, clearLocalState, isNotified]);
+    useEffect(() => {
+      if (state && !isNotified) {
+        countDown();
+      }
+    }, [state, message, clearLocalState, isNotified]);
 
-  return <>{contextHolder}</>;
+    return <>{contextHolder}</>;
+  }
 };
 
 export default Notification;
