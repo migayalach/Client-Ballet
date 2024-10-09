bf2953dfcc4035ece78eb09837c6070024ede22b
migayalach <ayalachavezmiguel@gmail.com>
2024-10-08 23:34:30 -0400

    button edit

--- client/src/components/button/buttonEdit/ButtonEdit.jsx
+++ client/src/components/button/buttonEdit/ButtonEdit.jsx
@@ -2,11 +2,21 @@ import React, { useEffect, useState } from "react";
 import { Button, Flex } from "antd";
 import Text from "../../text/Text";
 import EditModal from "@/components/modal/editModal/EditModal";
+import { EditOutlined } from "@ant-design/icons";
 
 function ButtonEdit({ idData, text, render }) {
+  const [flag, setFlag] = useState(false);
+
+  const handleChange = () => {   
+    setFlag(!flag);
+  };
+
   return (
     <Flex wrap="wrap" gap="small">
-      <EditModal idData={idData} text={text} render={render} />
+      <Button type="primary" onClick={handleChange} >
+        <EditOutlined />
+        {flag && <EditModal idData={idData} text={text} render={render} />}
+      </Button>
     </Flex>
   );
 }
