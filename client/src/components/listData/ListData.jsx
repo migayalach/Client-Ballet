7ef12ddb4cb3847a91ae18f657c8e38a2a045d01
migayalach <ayalachavezmiguel@gmail.com>
2024-10-09 22:11:31 -0400

    list data

--- client/src/components/listData/ListData.jsx
+++ client/src/components/listData/ListData.jsx
@@ -8,18 +8,26 @@ const ContainerHeight = 400;
 
 function ListData({ flagRender, modal }) {
   const selectFilterAll = useSelector(({ root }) => root?.filterAll);
+  const selectClassStudent = useSelector(({ root }) => root?.student);
   const [data, setData] = useState([]);
+
+  const filterStudents = (list, students) => {
+    for (let i = 0; i < students.length; i++) {
+      list = list.filter((index) => index.idUser !== students[i].idUser);
+    }    
+    return list;
+  };
 
   useEffect(() => {
     if (selectFilterAll) {
-      setData(selectFilterAll);
+      setData(filterStudents(selectFilterAll, selectClassStudent));
     }
   }, [selectFilterAll]);
 
   return (
     <List>
       <TableComponent
-        data={selectFilterAll}
+        data={data}
         render={flagRender}
         modal={modal}
       />
