f7beb0680fa3a4e142808e722cd1b244f456271c
migayalach <ayalachavezmiguel@gmail.com>
2024-10-18 22:19:07 -0400

    form class validation

---
+++ client/src/components/form/formClass/formClassValidation.js
@@ -0,0 +1,22 @@
+const formClassValidate = (data) => {
+  console.log(data);
+  
+  const errors = {};
+  if (data.idUser === 0) {
+    errors.user = "Por favor asigne un profesor a esta clase!";
+  }
+  if (data.idTypeClass === 0) {
+    errors.typeClass = "Por favor asigne una danza para la clase!";
+  }
+  if (data.idHours === 0) {
+    errors.hours = "Por favor asigne un horario a la clase";
+  }
+  
+  if (data.parallel.length < 3) {
+    errors.parallel = "Por favor ingrese un nombre para el paralelo!";
+  }
+
+  return errors;
+};
+
+export default formClassValidate;
