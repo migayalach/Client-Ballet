31a1c9b98230565bf01596328bdf43149a56777d
migayalach <ayalachavezmiguel@gmail.com>
2024-10-24 13:51:40 -0400

    [idQualification] page

--- client/src/app/class/qualification/[idQualification]/page.jsx
+++ client/src/app/class/qualification/[idQualification]/page.jsx
@@ -3,23 +3,31 @@
 // COMPONET'S
 import TableNotes from "@/components/tableComponent/TableNotes";
 import Page404 from "@/components/pageResult/Page404";
+import Notification from "@/components/modal/notification/Notification";
 
 // HOOK'S
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-// LIBRARY
-
 //REDUX
-import { getQualificationAll } from "@/redux/actions";
-
-// JAVASCRIP
-
-// STYLESHEET'
+import {
+  getQualificationAll,
+  qualificationClear,
+  removeAux,
+} from "@/redux/actions";
 
 function page({ params }) {
   const dispatch = useDispatch();
+  const [dataState, setDataState] = useState({});
+  const [flagAlert, setFlagAlert] = useState(false);
   const selectUser = useSelector(({ root }) => root?.access);
+  const selectState = useSelector(({ root }) => root?.state);
+  const selectError = useSelector(({ root }) => root?.error);
+
+  const clearLocalState = () => {
+    setDataState({});
+    setFlagAlert(false);
+  };
 
   if (Object.keys(selectUser).length === 0) {
     return (
@@ -31,12 +39,42 @@ function page({ params }) {
 
   useEffect(() => {
     dispatch(getQualificationAll(params?.idQualification));
+    return () => {
+      dispatch(qualificationClear());
+      dispatch(removeAux());
+    };
   }, []);
+
+  useEffect(() => {
+    if (selectState === "create-qualification") {
+      setDataState({
+        action: "create-qualification",
+        state: "success",
+        message: "Calificacion realizada con exito!",
+      });
+    } else if (selectError?.error === "No se pudo realizar esta operacion!") {
+      setDataState({
+        action: "error-delete-qualification",
+        state: "error",
+        message: selectError?.error,
+      });
+    }
+    setFlagAlert(true);
+  }, [selectState, selectError]);
 
   return (
     <div>
       <h1>Lista de estudiantes</h1>
+
       <TableNotes />
+      <div>
+        {flagAlert && (
+          <Notification
+            dataState={dataState}
+            clearLocalState={clearLocalState}
+          />
+        )}
+      </div>
     </div>
   );
 }
