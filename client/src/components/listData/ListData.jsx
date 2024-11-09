2271421be12ca27651f1975004d3e081c5c84013
migayalach <ayalachavezmiguel@gmail.com>
2024-11-09 12:05:00 -0400

    list data

--- client/src/components/listData/ListData.jsx
+++ client/src/components/listData/ListData.jsx
@@ -1,27 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Avatar, List, message } from "antd";
-import VirtualList from "rc-virtual-list";
+import { List } from "antd";
 import TableComponent from "../tableComponent/TableComponent";
 
 const ContainerHeight = 400;
 
 function ListData({ flagRender, modal }) {
+  
   const selectFilterAll = useSelector(({ root }) => root?.filterAll);
-  const selectClassStudent = useSelector(({ root }) => root?.student);
   const [data, setData] = useState([]);
 
-  const filterStudents = (list, students) => {
-    for (let i = 0; i < students.length; i++) {
-      list = list.filter((index) => index.idUser !== students[i].idUser);
-    }
-    return list;
-  };
-
   useEffect(() => {
-    // if (selectFilterAll) {
-    //   setData(filterStudents(selectFilterAll, selectClassStudent));
-    // }
     setData(selectFilterAll);
   }, [selectFilterAll]);
 
